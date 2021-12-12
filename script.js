d440cefd4e092e553ca971024b7ade0764280f27
joao vitor <joao.vitor13mendes@gmail.com>
2021-12-12 10:43:33 -0300

    Primeiro commit

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 $(document).ready(function(){
     $(window).scroll(function(){
-        // sticky navbar on scroll script
+
         if(this.scrollY > 20){
             $('.navbar').addClass("sticky");
         }else{
             $('.navbar').removeClass("sticky");
         }
         
-        // scroll-up button show/hide script
+
         if(this.scrollY > 500){
             $('.scroll-up-btn').addClass("show");
         }else{
@@ -15,15 +15,14 @@ $(document).ready(function(){
         }
     });
 
-    // slide-up script
     $('.scroll-up-btn').click(function(){
         $('html').animate({scrollTop: 0});
-        // removing smooth scroll on slide-up button click
+
         $('html').css("scrollBehavior", "auto");
     });
 
     $('.navbar .menu li a').click(function(){
-        // applying again smooth scroll on menu items click
+
         $('html').css("scrollBehavior", "smooth");
     });
 
